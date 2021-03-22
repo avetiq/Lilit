4f97a17e1db4d7ed3f0dc59ef6522c80b07e37e5
avetiq <avo6385@gmail.com>
2021-03-22 10:42:01 +0400

    pageLinks

--- src/components/pages/Result/index.js
+++ src/components/pages/Result/index.js
@@ -2,33 +2,56 @@ import React from 'react';
 import SearchHotel from '../../SearchHotel';
 import styles from './styles';
 import { withStyles } from '@material-ui/core/styles';
-
+import { Button } from 'react-bootstrap';
 
 
 function Result(props){
     
     const {classes} = props;
     const initialSearchValues = props.location.query;
+    const [hotelSearch, setHotelSearch] = React.useState(true);
+    const [viewSearch, setViewSearch] = React.useState(false);
     
     return (
         <div className={classes.main}>
             <div className={classes.headerLinks}>
                 <div className={classes.headerLinksColor}>
-                    <h3>Hotels</h3>
+                    <Button
+                    variant="success"
+                    className={`${classes.buttonPadding} ${hotelSearch ? classes.buttonHeightMax : classes.buttonHeightMin}`}
+                    onClick={()=> {
+                        if(!hotelSearch){
+                            setHotelSearch(!hotelSearch); setViewSearch(!viewSearch);
+                        }
+                    }}
+                    >
+                        <p className={`${hotelSearch ? classes.linkBackground : ''}`}>Hotels</p>
+                    </Button>
                 </div>
                 <div className={classes.headerLinksColor}>
-                    <h3>Views</h3>
+                    <Button
+                    className={`${classes.buttonPadding} ${viewSearch ? classes.buttonHeightMax : classes.buttonHeightMin}`}
+                    variant="success"
+                    onClick={()=> {
+                        if(!viewSearch){
+                            setViewSearch(!viewSearch); setHotelSearch(!hotelSearch);
+                        }
+                    }}
+                    >
+                        <p className={`${viewSearch ? classes.linkBackground : ''}`}>Views</p>
+                    </Button>    
                 </div>
             </div>
             <div className={classes.formInputs}>
-                <SearchHotel
+                {hotelSearch && <SearchHotel
                     hotel={initialSearchValues.hotel}
                     view={initialSearchValues.view}
                     district={initialSearchValues.district}
                     bed={initialSearchValues.bed}
                     from={initialSearchValues.from}
                     to={initialSearchValues.to}
-                />
+                />}
+                {viewSearch && <p>stex el en myus formy klini</p>}
             </div>
         </div>
     );
