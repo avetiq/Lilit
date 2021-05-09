e1b233d3237273ae530e526ecb4d1bef6a19d8c6
GogiLoloyan <gogi.l@hilearn.io>
2021-05-09 02:04:21 -0400

    from Gogi

--- src/components/pages/Hotel/styles.js
+++ src/components/pages/Hotel/styles.js
@@ -1,59 +1,75 @@
 const styles = (theme) => ({
-    main: {
-        display: 'flex',
-        alignItems: 'center',
-        flexDirection: 'column'
+  main: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  contentPart: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: 30,
+    marginBottom: 20,
+  },
+  headerContent: {
+    marginTop: 25,
+    width: "42%",
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+  content: {
+    width: "37%",
+    marginRight: 20,
+  },
+  slider: {
+    "&>ul": {
+      marginBottom: "80px",
     },
-    contentPart: {
-        display: 'flex',
-        justifyContent: 'center',
-        marginTop: 30,
-        marginBottom: 20
+    "&>svg": {
+      color: "aliceblue",
+      zIndex: 10,
+      fontSize: 74,
     },
-    headerContent: {
-        marginTop: 25,
-        width: '42%',
-        display: 'flex',
-        justifyContent: 'center',
-        flexDirection: 'column'
-    },
-    content: {
-        width: '37%',
-        marginRight: 20
-    },
-    slider: {
-        '&>ul': {
-            marginBottom: '80px',
-        },
-        '&>svg': {
-            color: "aliceblue",
-            zIndex: 10,
-            fontSize: 74,
-        }, 
-    },
-    freeRooms: {
-        marginBottom: 100
-    },
-    freeRoom: {
-        width: '100%',
-        height: 50,
-        display: 'flex',
-        marginBottom: 10,
-        justifyContent: 'space-between'
-    },
-    dates: {
-        display: 'flex',
-        justifyContent: 'space-between',
-    },
-    bedImg: {
-        width: 50,
-        height: 50,
-        backgroundSize: 'contain',
-        backgroundRepeat: 'no-repeat',
-        backgroundPositionX: 'center'
-    }
-    
+    "& .slick-prev": {
+      left: "-10px",
+      transition: "left 0.2s ease",
 
+      "&:hover": {
+        color: "white",
+        left: "-20px",
+      },
+    },
+    "& .slick-next": {
+      right: "-10px",
+      transition: "right 0.2s ease",
+
+      "&:hover": {
+        color: "white",
+        right: "-20px",
+      },
+    },
+  },
+  freeRooms: {
+    marginBottom: 100,
+  },
+  freeRoom: {
+    width: "100%",
+    height: 50,
+    display: "flex",
+    marginBottom: 10,
+    justifyContent: "space-between",
+  },
+  dates: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  bedImg: {
+    width: 50,
+    height: 50,
+    backgroundSize: "contain",
+    backgroundRepeat: "no-repeat",
+    backgroundPositionX: "center",
+  },
 });
 
 export default styles;
--- src/components/pages/Result/index.js
+++ src/components/pages/Result/index.js
@@ -1,160 +1,177 @@
-import React from 'react';
-import SearchHotel from '../../SearchHotel';
-import ViewSearch from '../../ViewSearch';
-import HotelResult from '../../HotelResult';
-import ViewResult from '../../ViewResult';
-import styles from './styles';
-import { withStyles } from '@material-ui/core/styles';
-import { Button } from 'react-bootstrap';
-import searchResult from '../../../models/searchResult';
-import DateUtil from '../../../helpers/DateUtil';
-import idGenerator from '../../../helpers/idGenerator';
-import Spinner from '../../Spinner';
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router";
 
-function Result(props){
-    
-    const {classes} = props;
-    const initialSearchValues = props.location.query;
-    const [hotelSearch, setHotelSearch] = React.useState(true);
-    const [viewSearch, setViewSearch] = React.useState(false);
-    const [hotelList, setHotelList] = React.useState();
-    const [viewList, setViewList] = React.useState();
-    const [hotelFreeRooms, setHotelFreeRooms] = React.useState();
-    const [spinner, setSpinner] = React.useState(true);
-    
-    React.useEffect(()=>{
+import SearchHotel from "../../SearchHotel";
+import ViewSearch from "../../ViewSearch";
+import HotelResult from "../../HotelResult";
+import ViewResult from "../../ViewResult";
+import styles from "./styles";
+import { withStyles } from "@material-ui/core/styles";
+import { Button } from "react-bootstrap";
+import searchResult from "../../../models/searchResult";
+import DateUtil from "../../../helpers/DateUtil";
+import idGenerator from "../../../helpers/idGenerator";
+import objectFromSearchParams from "../../../helpers/objectFromSearchParams";
+import Spinner from "../../Spinner";
 
-        let initialHotelFreeRooms;
-        initialHotelFreeRooms = {
-            from: DateUtil.isDate(initialSearchValues.from) ? DateUtil.formatDate(initialSearchValues.from): '',
-            to: DateUtil.isDate(initialSearchValues.to) ? DateUtil.formatDate(initialSearchValues.to) : '',
-            bed: initialSearchValues.bed
+function Result({ classes }) {
+  const [hotelSearch, setHotelSearch] = useState(true);
+  const [viewSearch, setViewSearch] = useState(false);
+  const [hotelList, setHotelList] = useState();
+  const [viewList, setViewList] = useState();
+  const [hotelFreeRooms, setHotelFreeRooms] = useState();
+  const [spinner, setSpinner] = useState(true);
+
+  const { search } = useLocation();
+
+  const initialSearchValues = objectFromSearchParams(search);
+
+  console.log(initialSearchValues);
+
+  useEffect(() => {
+    const initialHotelFreeRooms = {
+      from: DateUtil.isDate(initialSearchValues.from)
+        ? DateUtil.formatDate(initialSearchValues.from)
+        : "",
+      to: DateUtil.isDate(initialSearchValues.to)
+        ? DateUtil.formatDate(initialSearchValues.to)
+        : "",
+      bed: initialSearchValues.bed,
+    };
+    setHotelFreeRooms(initialHotelFreeRooms);
+
+    fetch(
+      `/api/Travel?` +
+        new URLSearchParams({
+          HotelName: initialSearchValues.hotel,
+          ViewName: initialSearchValues.view,
+          District: initialSearchValues.district,
+          From: initialSearchValues.from
+            ? DateUtil.formatDate(initialSearchValues.from)
+            : "",
+          To: initialSearchValues.to
+            ? DateUtil.formatDate(initialSearchValues.to)
+            : "",
+          BedQuantity: initialSearchValues.bed
+            ? parseInt(initialSearchValues.bed)
+            : "",
+        }),
+      {
+        method: "GET",
+      }
+    )
+      .then(async (response) => {
+        const res = await response.json();
+
+        if (response.status >= 400 && response.status < 600) {
+          if (res.error) {
+            throw res.error;
+          } else {
+            throw new Error("Something went wrong!");
+          }
         }
-        setHotelFreeRooms(initialHotelFreeRooms);
+        return res;
+      })
+      .then((res) => {
+        const dataHotel = [];
+        const dataView = [];
+        res.forEach((resElement) => {
+          const current = new searchResult();
+          current.id = resElement.id;
+          current.name = resElement.name;
+          current.district = resElement.district;
+          current.photoSource = resElement.photoSource;
+          current.longInfo = resElement.longInfo;
+          current.latitude = resElement.latitude;
+          current.longitude = resElement.longitude;
+          if (resElement.isHotel) {
+            dataHotel.push(current);
+          } else {
+            dataView.push(current);
+          }
+        });
 
-        fetch(`/api/Travel?`+ new URLSearchParams({
-            HotelName: initialSearchValues.hotel,
-            ViewName: initialSearchValues.view,
-            District: initialSearchValues.district,
-            From: initialSearchValues.from ? DateUtil.formatDate(initialSearchValues.from) : '',
-            To: initialSearchValues.to ? DateUtil.formatDate(initialSearchValues.to) : '',
-            BedQuantity: initialSearchValues.bed ? parseInt(initialSearchValues.bed) : '',
-        }), {
-            method: 'GET',
-            
-        })
-            .then(async (response) => {
-                const res = await response.json();
+        setHotelList(dataHotel);
+        setViewList(dataView);
+        setSpinner(false);
+      })
+      .catch((error) => {
+        console.log("catch error", error);
+      });
+  }, []);
 
-                if(response.status >=400 && response.status < 600){
-                    if(res.error){
-                        throw res.error;
-                    }
-                    else {
-                        throw new Error('Something went wrong!');
-                    }
-                }
-                return res;
-            })
-            .then((res) =>{
-                const dataHotel: Array<searchResult> = [];
-                const dataView: Array<searchResult> = [];
-                res.forEach(resElement => {
-                    const current = new searchResult();
-                    current.id = resElement.id;
-                    current.name = resElement.name;
-                    current.district = resElement.district;
-                    current.photoSource = resElement.photoSource;
-                    current.longInfo = resElement.longInfo;
-                    current.latitude = resElement.latitude;
-                    current.longitude = resElement.longitude;
-                    if(resElement.isHotel){
-                        dataHotel.push(current);
-                    }else{
-                        dataView.push(current);
-                    }
-                    
-                });
-                
-                setHotelList(dataHotel);
-                setViewList(dataView);
-                setSpinner(false);
-            })
-            .catch((error)=>{
-                console.log('catch error', error);
-            });
-
-    }, [])
-
-    return (
-        <div className={classes.main}>
-            <div className={classes.headerLinks}>
-                <div className={classes.headerLinksColor}>
-                    <Button
-                    variant="success"
-                    className={`${classes.buttonPadding} ${hotelSearch ? classes.buttonHeightMax : classes.buttonHeightMin}`}
-                    onClick={()=> {
-                        if(!hotelSearch){
-                            setHotelSearch(!hotelSearch); setViewSearch(!viewSearch);
-                        }
-                    }}
-                    >
-                        <p className={`${hotelSearch ? classes.linkBackground : ''}`}>Հյուրանոցներ</p>
-                    </Button>
-                </div>
-                <div className={classes.headerLinksColor}>
-                    <Button
-                    className={`${classes.buttonPadding} ${viewSearch ? classes.buttonHeightMax : classes.buttonHeightMin}`}
-                    variant="success"
-                    onClick={()=> {
-                        if(!viewSearch){
-                            setViewSearch(!viewSearch); setHotelSearch(!hotelSearch);
-                        }
-                    }}
-                    >
-                        <p className={`${viewSearch ? classes.linkBackground : ''}`}>Տեսարժան վայրեր</p>
-                    </Button>    
-                </div>
-            </div>
-            <div className={classes.formInputs}>
-                {hotelSearch && <SearchHotel
-                    hotel={initialSearchValues.hotel}
-                    district={initialSearchValues.district}
-                    bed={initialSearchValues.bed}
-                    from={initialSearchValues.from}
-                    to={initialSearchValues.to}
-                />}
-                {viewSearch && <ViewSearch
-                    view={initialSearchValues.view}
-                    district={initialSearchValues.district}
-                    />}
-            </div>
-            {
-                hotelSearch && hotelList &&
-                hotelList.map((el) => 
-                    <HotelResult
-                        key={idGenerator()}
-                        info={el}
-                        hotelFreeRooms={hotelFreeRooms}
-                    />
-                )
-            }
-            {
-                viewSearch && viewList &&
-                viewList.map((el) => 
-                    <ViewResult
-                        key={idGenerator()}
-                        info={el}
-                    />
-                )
-            }
-            {
-                spinner && <Spinner />
-            }
+  return (
+    <div className={classes.main}>
+      <div className={classes.headerLinks}>
+        <div className={classes.headerLinksColor}>
+          <Button
+            variant="success"
+            className={`${classes.buttonPadding} ${
+              hotelSearch ? classes.buttonHeightMax : classes.buttonHeightMin
+            }`}
+            onClick={() => {
+              if (!hotelSearch) {
+                setHotelSearch(!hotelSearch);
+                setViewSearch(!viewSearch);
+              }
+            }}
+          >
+            <p className={`${hotelSearch ? classes.linkBackground : ""}`}>
+              Հյուրանոցներ
+            </p>
+          </Button>
         </div>
-    );
-
-
+        <div className={classes.headerLinksColor}>
+          <Button
+            className={`${classes.buttonPadding} ${
+              viewSearch ? classes.buttonHeightMax : classes.buttonHeightMin
+            }`}
+            variant="success"
+            onClick={() => {
+              if (!viewSearch) {
+                setViewSearch(!viewSearch);
+                setHotelSearch(!hotelSearch);
+              }
+            }}
+          >
+            <p className={`${viewSearch ? classes.linkBackground : ""}`}>
+              Տեսարժան վայրեր
+            </p>
+          </Button>
+        </div>
+      </div>
+      <div className={classes.formInputs}>
+        {hotelSearch && (
+          <SearchHotel
+            hotel={initialSearchValues.hotel}
+            district={initialSearchValues.district}
+            bed={initialSearchValues.bed}
+            from={initialSearchValues.from}
+            to={initialSearchValues.to}
+          />
+        )}
+        {viewSearch && (
+          <ViewSearch
+            view={initialSearchValues.view}
+            district={initialSearchValues.district}
+          />
+        )}
+      </div>
+      {hotelSearch &&
+        hotelList &&
+        hotelList.map((el) => (
+          <HotelResult
+            key={idGenerator()}
+            info={el}
+            hotelFreeRooms={hotelFreeRooms}
+          />
+        ))}
+      {viewSearch &&
+        viewList &&
+        viewList.map((el) => <ViewResult key={idGenerator()} info={el} />)}
+      {spinner && <Spinner />}
+    </div>
+  );
 }
 
 export default withStyles(styles)(Result);
